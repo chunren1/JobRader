eea0fbd5e808c072045a919119e7d8661c2c1ded
kkStar <1273616219@qq.com>
2026-06-28 00:08:58 +0800

    fix: CORS 改为 * 解决扩展跨域

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'chrome-extension://*' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, X-Local-Secret' },
         ],
